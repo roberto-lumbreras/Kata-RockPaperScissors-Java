9aea63e2692b100e7dd2a2fb7f5686f1dc1778b1
roberto-lumbreras <rlumbrerascanal@gmail.com>
2024-11-17 18:55:43 +0100

    Lizard and Spock implementation and testing

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,8 +1,10 @@
 package com.factoriaf5.rps.application;
 
+import com.factoriaf5.rps.models.Lizard;
 import com.factoriaf5.rps.models.Paper;
 import com.factoriaf5.rps.models.Rock;
 import com.factoriaf5.rps.models.Scissors;
+import com.factoriaf5.rps.models.Spock;
 
 public class Game {
     public Object evaluateGameWinner(Object o1,Object o2){
@@ -13,11 +15,44 @@ public class Game {
         null:
         o1 instanceof Scissors && o2 instanceof Paper?
         o1:
+        o1 instanceof Scissors && o2 instanceof Lizard?
+        o1:
         o1 instanceof Paper && o2 instanceof Rock?
+        o1:
+        o1 instanceof Paper && o2 instanceof Spock?
         o1:
         o1 instanceof Rock && o2 instanceof Scissors?
         o1:
-        o1.getClass() == o2.getClass()?
-        null:o2;
+        o1 instanceof Rock && o2 instanceof Lizard?
+        o1:
+        o1 instanceof Lizard && o2 instanceof Spock?
+        o1:
+        o1 instanceof Lizard && o2 instanceof Paper?
+        o1:
+        o1 instanceof Spock && o2 instanceof Scissors?
+        o1:
+        o1 instanceof Spock && o2 instanceof Rock?
+        o1:
+        o2 instanceof Scissors && o1 instanceof Paper?
+        o2:
+        o2 instanceof Scissors && o1 instanceof Lizard?
+        o2:
+        o2 instanceof Paper && o1 instanceof Rock?
+        o2:
+        o2 instanceof Paper && o1 instanceof Spock?
+        o2:
+        o2 instanceof Rock && o1 instanceof Scissors?
+        o2:
+        o2 instanceof Rock && o1 instanceof Lizard?
+        o2:
+        o2 instanceof Lizard && o1 instanceof Spock?
+        o2:
+        o2 instanceof Lizard && o1 instanceof Paper?
+        o2:
+        o2 instanceof Spock && o1 instanceof Scissors?
+        o2:
+        o2 instanceof Spock && o1 instanceof Rock?
+        o2:
+        null;
     }
 }
--- src/test/java/com/factoriaf5/rps/application/GameTest.java
+++ src/test/java/com/factoriaf5/rps/application/GameTest.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.factoriaf5.rps.models.Lizard;
 import com.factoriaf5.rps.models.Paper;
 import com.factoriaf5.rps.models.Rock;
 import com.factoriaf5.rps.models.Scissors;
+import com.factoriaf5.rps.models.Spock;
 
 public class GameTest {
 
@@ -25,7 +27,7 @@ public class GameTest {
     }
 
     @Test
-    public void ScissorsWinsPaper() {
+    public void o1ScissorsWinsPaper() {
         o1 = new Scissors();
         o2 = new Paper();
         winner = game.evaluateGameWinner(o1, o2);
@@ -33,7 +35,15 @@ public class GameTest {
     }
 
     @Test
-    public void RockWinsScissors() {
+    public void o1ScissorsWinsLizard() {
+        o1 = new Scissors();
+        o2 = new Lizard();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o1RockWinsScissors() {
         o1 = new Rock();
         o2 = new Scissors();
         winner = game.evaluateGameWinner(o1, o2);
@@ -41,7 +51,15 @@ public class GameTest {
     }
 
     @Test
-    public void PaperWinsRock() {
+    public void o1RockWinsLizard() {
+        o1 = new Rock();
+        o2 = new Lizard();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o1PaperWinsRock() {
         o1 = new Paper();
         o2 = new Rock();
         winner = game.evaluateGameWinner(o1, o2);
@@ -49,22 +67,135 @@ public class GameTest {
     }
 
     @Test
-    public void sameHandCausesDraw() {
+    public void o1PaperWinsSpock() {
         o1 = new Paper();
-        o2 = new Paper();
-        assertTrue(game.evaluateGameWinner(o1, o2)==null);
+        o2 = new Spock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
     }
 
     @Test
-    public void ifO1isNotWinnerAndThereIsNoDraw_O2Wins() {
-        o1 = new Rock();
+    public void o1LizardWinsSpock() {
+        o1 = new Lizard();
+        o2 = new Spock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o1LizardWinsPaper() {
+        o1 = new Lizard();
         o2 = new Paper();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o1SpockWinsRock() {
+        o1 = new Spock();
+        o2 = new Rock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o1SpockWinsScissors() {
+        o1 = new Spock();
+        o2 = new Scissors();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o1, winner);
+    }
+
+    @Test
+    public void o2ScissorsWinsPaper() {
+        o2 = new Scissors();
+        o1 = new Paper();
         winner = game.evaluateGameWinner(o1, o2);
         assertEquals(o2, winner);
     }
 
     @Test
-    public void ifAnyHandIsNull_thereIsNoWinner() {
+    public void o2ScissorsWinsLizard() {
+        o2 = new Scissors();
+        o1 = new Lizard();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2RockWinsScissors() {
+        o2 = new Rock();
+        o1 = new Scissors();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2RockWinsLizard() {
+        o2 = new Rock();
+        o1 = new Lizard();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2PaperWinsRock() {
+        o2 = new Paper();
+        o1 = new Rock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2PaperWinsSpock() {
+        o2 = new Paper();
+        o1 = new Spock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2LizardWinsSpock() {
+        o2 = new Lizard();
+        o1 = new Spock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2LizardWinsPaper() {
+        o2 = new Lizard();
+        o1 = new Paper();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2SpockWinsRock() {
+        o2 = new Spock();
+        o1 = new Rock();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2SpockWinsScissors() {
+        o2 = new Spock();
+        o1 = new Scissors();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertEquals(o2, winner);
+    }
+
+    @Test
+    public void o2ifO1isNotWinnerAndO2isNotWinner_ThereIsADraw() {
+        o1 = new Paper();
+        o2 = new Paper();
+        winner = game.evaluateGameWinner(o1, o2);
+        assertTrue(null == winner);
+    }
+
+    @Test
+    public void o2ifAnyHandIsNull_thereIsNoWinner() {
         o2 = new Paper();
         winner = game.evaluateGameWinner(o1, o2);
         assertTrue(null == winner);
